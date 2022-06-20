373220b2f158ae954d9b60561c4bf46b1b7d9ccd
Ion Bold <bold.ion86@gmail.com>
2022-06-20 22:51:20 +0300

    Receiving massages and sending responses.
    Signed-off-by: Ion Bold <bold.ion86@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "messages.h"
 #include <RH_ASK.h>
 #ifdef RH_HAVE_HARDWARE_SPI
-#include <SPI.h> // Not actually used but needed to compile
+#include <SPI.h>                  // Not actually used but needed to compile
 #endif
 
-RH_ASK driver(2000, 11, 12, 13); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
+#define MY_ADDRESS    0x01        // This board destination address
+
+RH_ASK driver(2000, 11, 12, 13);  // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
 
 void setup()
 {
@@ -25,9 +27,36 @@ void loop()
   uint8_t buf[COMMAND_SIZE];
   uint8_t buflen = sizeof(buf);
 
+
   if (driver.recv(buf, &buflen)) // Non-blocking
   {
-    // Message with a good checksum received, dump it.
-    driver.printBuffer("Got:", buf, buflen);
+    Command recvCommand(buf);
+    if(recvCommand.destAddress == MY_ADDRESS)
+    {
+      Response_TypeDef r = ACK;
+      uint32_t p = 0;
+      switch (recvCommand.cmd)
+      {
+      case GET_TEMPERATURE:
+        Serial.println("Temperature command received.");
+        r = ACK;
+        p = 27;
+        break;
+      case GET_HUMIDITY:
+        Serial.println("Humidity command received.");
+        r = ACK;
+        p = 80;
+        break;
+      default:
+        Serial.print("Unknown command.");
+        r = NACK;
+        p = 0;
+        break;
+      }
+      Response myResponse(r, p);
+      // TODO: Review add payload to response and maybe set crc
+      driver.send(myResponse.responseArray, RESPONSE_SIZE);
+      driver.printBuffer("Sent: ", myResponse.responseArray, RESPONSE_SIZE);
+    }
   }
 }
