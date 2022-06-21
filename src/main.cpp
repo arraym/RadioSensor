1492bd869f626d010810e2b6cd4a61009bd45771
Ion Bold <bold.ion86@gmail.com>
2022-06-21 23:24:16 +0300

    DHT sensor library added, sensor values are now sent to server.
    Signed-off-by: Ion Bold <bold.ion86@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 #include "messages.h"
+#include "DHT.h"
 #include <RH_ASK.h>
 #ifdef RH_HAVE_HARDWARE_SPI
-#include <SPI.h>                  // Not actually used but needed to compile
+#include <SPI.h>                // Not actually used but needed to compile
 #endif
 
-#define MY_ADDRESS    0x01        // This board destination address
+#define MY_ADDRESS    0x01      // This board destination address
 
-RH_ASK driver(2000, 11, 12, 13);  // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
+// Radio driver defines
+#define RX_PIN      11
+#define TX_PIN      12
+#define PTT_PIN     13          // (unused)
+
+RH_ASK driver(2000, RX_PIN, TX_PIN, PTT_PIN);  
+                                // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
+DHT dht;
 
 void setup()
 {
 #ifdef RH_HAVE_SERIAL
-  Serial.begin(115200);	  // Debugging only
+  Serial.begin(115200);	        // Debugging only
 #endif
   if (!driver.init())
 #ifdef RH_HAVE_SERIAL
@@ -20,13 +28,14 @@ void setup()
 #else
 	;
 #endif
+  dht.setup(10);
+  delay(dht.getMinimumSamplingPeriod());
 }
 
 void loop()
 {
   uint8_t buf[COMMAND_SIZE];
   uint8_t buflen = sizeof(buf);
-
 
   if (driver.recv(buf, &buflen)) // Non-blocking
   {
@@ -38,15 +47,25 @@ void loop()
       switch (recvCommand.cmd)
       {
       case GET_TEMPERATURE:
-        Serial.println("Temperature command received.");
+      {
+        float temperature = dht.getTemperature();
+        Serial.print("Temperature: ");
+        Serial.print(temperature, 2);
+        Serial.println("*C");
         r = ACK;
-        p = 27;
+        p = (uint32_t)(temperature * 100);
         break;
+      }
       case GET_HUMIDITY:
-        Serial.println("Humidity command received.");
+      {
+        float humidity = dht.getHumidity();
+        Serial.print("Humidity: ");
+        Serial.print(humidity, 2);
+        Serial.println("%");
         r = ACK;
-        p = 80;
+        p = (uint32_t)(humidity * 100);
         break;
+      }
       default:
         Serial.print("Unknown command.");
         r = NACK;
@@ -55,7 +74,6 @@ void loop()
       }
       Response myResponse(r, p);
       driver.send(myResponse.responseArray, RESPONSE_SIZE);
-      driver.printBuffer("Sent: ", myResponse.responseArray, RESPONSE_SIZE);
     }
   }
 }
